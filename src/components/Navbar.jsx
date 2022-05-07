b015d27668087706ff35a04dcb19f296f03802c2
rohitsurya99 <infiniterohitsurya@gmail.com>
2022-05-08 03:43:24 +0530

    ADDED LOGO

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='fixed items-center flex justify-between w-full h-[80px] bg-[#0a192f] px-4 text-gray-300'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
