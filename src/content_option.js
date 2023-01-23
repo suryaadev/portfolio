bc8c2f1ec6a6c12f7ce41a874b127b92f52d4c7c
suryaadev <infiniterohitsurya@gmail.com>
2023-01-24 01:47:22 +0530

    inital commit

---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "SURYAADEV";
+const meta = {
+  title: "Rohit Suryawanshi",
+  description:
+    "I’m Rohit Suryawanshi Blockchain Developer Looking for Web3 opportunities",
+};
+
+const introdata = {
+  title: "I’m Rohit Suryawanshi",
+  animated: {
+    first: "I love Blockchain",
+    second: "I Develope Dapps",
+    third: "Talks about WEB 3.0",
+  },
+  description:
+    "I have experience in creating smart contracts, building decentralized applications, and implementing blockchain solutions for various use cases. My knowledge of Solidity, Ethereum, and other blockchain platforms make me a valuable asset to any team.",
+  your_img_url: "./1.png",
+};
+
+const dataabout = {
+  title: "About Me",
+  aboutme:
+    "Self taught blockchain developer. Experience in developing complex smart contracts. Worked before on EVM based chain. Having good understanding of DeFi, NFT and tokenomics and other L2 scaling solutions",
+};
+const worktimeline = [
+  {
+    jobtitle: "Systems Engineer",
+    where: "Infosys",
+    date: "Sep 2021 - Present",
+  },
+  {
+    jobtitle: "React Intern",
+    where: "Mach15",
+    date: "2020",
+  },
+  {
+    jobtitle: "Front-End Intern",
+    where: "IT Planet",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Solidity",
+    value: 88,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Express",
+    value: 72,
+  },
+
+  {
+    name: "React",
+    value: 68,
+  },
+  {
+    name: "Postgresql",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "./chai_.png",
+    desctiption:
+      "Buy Me a chai is a platform for accepting crypto payments as a token of motivation and show them on home with given message by user.",
+    link: "https://suryachai.netlify.app/",
+    code: "https://github.com/suryaadev/BuyMeChai.git",
+  },
+  {
+    img: "./IPFS.png",
+    desctiption:
+      "Web 3 drive where user can upload his images on decentralized platform using pinata and alsp can share with other users too",
+    link: "https://suryadrive.netlify.app/",
+    code: "https://github.com/suryaadev/IPFS_drive.git",
+  },
+
+  {
+    img: "whitelist.png",
+    desctiption:
+      "Before minting NFT whitlist dapp where only 20 users can whitelist there address",
+    link: "https://whitelist-dapp-jsqcjdimy-suryaadev.vercel.app/",
+    code: "https://github.com/suryaadev/whitelist_Dapp.git",
+  },
+  {
+    img: "NFT.png",
+    desctiption:
+      "ERC721 NFT contract where whitelist user mint NFTs during presale and other users can mint after presale over",
+    link: "https://suryanftcollection.netlify.app/",
+    code: "https://github.com/suryaadev/NFT_Collection.git",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "rsurya1760@gmail.com",
+  YOUR_FONE: "+91 7028182539",
+  description: "Let's work together to create a decentralized future",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_e1mo9xf",
+  YOUR_TEMPLATE_ID: "template_mj4liwd",
+  YOUR_USER_ID: "HQFXotkAE6cC9QtpU",
+};
+
+const socialprofils = {
+  github: "https://github.com/suryaadev",
+  linkedin: "https://linkedin.com/in/suryaadev",
+  twitter: "https://twitter.com/suryaadev_",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
